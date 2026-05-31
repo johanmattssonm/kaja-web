e1db5b4ca184ce2482be6c2241ae9df9c283bf07
Johan Mattsson <johan.mattsson.m@gmail.com>
2026-06-01 00:10:14 +0200

    Cleanup

--- examples/clickcounter/main.js
+++ examples/clickcounter/main.js
@@ -4,23 +4,15 @@ async function loadWasm() {
   try {
     const wasmModule = await import("./clickcounter.js");
     await wasmModule.default("./clickcounter_bg.wasm");
-    await wasmModule.initCallbacks();
+
     return wasmModule;
   } catch (error) {
     console.error("Failed to load WASM module:", error);
-    return null;
   }
+
+  return null;
 }
 
 window.addEventListener("DOMContentLoaded", async () => {
   wasmModule = await loadWasm();
-
-  window.realjs = function () {
-    console.log("RealJS");
-
-    wasmModule.test();
-    //wasmModule.incrementCounter();
-  };
-
-  window.runTest = wasmModule.test;
 });
