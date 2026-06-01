709ebd29c1e67149de2a719f9543980f7251783e
Johan Mattsson <johan.mattsson.m@gmail.com>
2026-06-01 20:33:25 +0200

    Four spaces for tabs

--- examples/callbacks/loader.js
+++ examples/callbacks/loader.js
@@ -1,18 +1,18 @@
 let wasmModule;
 
 async function loadWasm() {
-  try {
-    const wasmModule = await import("./main.js");
-    await wasmModule.default("./main_bg.wasm");
+    try {
+        const wasmModule = await import("./main.js");
+        await wasmModule.default("./main_bg.wasm");
 
-    return wasmModule;
-  } catch (error) {
-    console.error("Failed to load WASM module:", error);
-  }
+        return wasmModule;
+    } catch (error) {
+        console.error("Failed to load WASM module:", error);
+    }
 
-  return null;
+    return null;
 }
 
 window.addEventListener("DOMContentLoaded", async () => {
-  wasmModule = await loadWasm();
+    wasmModule = await loadWasm();
 });
