feeb09134ddcb7938176287b314ec367a79ae7d3
Johan Mattsson <johan.mattsson.m@gmail.com>
2026-06-01 23:33:56 +0200

    Color in example

--- examples/callbacks/src/lib.rs
+++ examples/callbacks/src/lib.rs
@@ -1,4 +1,3 @@
-#![cfg(target_arch = "wasm32")]
 use kaja_web::prelude::*;
 use serde::Deserialize;
 
@@ -17,7 +16,7 @@ fn update() {
             Test 1
         </button>
 
-        <button onclick="test2({test_parameter: 'just a test'}, 'string_param')">
+        <button onclick="test2({test_parameter: 'just a test'}, 'string_param'">
             Test 2
         </button>
 
@@ -33,14 +32,24 @@ fn update() {
     }
 }
 
+fn set_color(color: &str) {
+    let main = get_element("#main-view");
+
+    if let Some(main) = main {
+        let _ = main.style().set_property("background-color", color);
+    }
+}
+
 #[callback(test0)]
 pub fn test0() {
     log!("test0 called");
+    set_color("red");
 }
 
 #[callback(test1)]
 pub fn test1(arg0_rust: Test1Data) {
     log!("test1 called with parameter: {}", arg0_rust.test_parameter);
+    set_color("blue");
 }
 
 #[callback(test2)]
@@ -50,11 +59,13 @@ pub fn test2(arg0_rust: Test1Data, arg1_rust: String) {
         arg0_rust.test_parameter,
         arg1_rust
     );
+    set_color("green");
 }
 
 #[callback(test3)]
 pub fn test3(a: u32, b: u32, c: u32) {
     log!("test3 {}, {} and {}", a, b, c);
+    set_color("pink");
 }
 
 #[wasm_bindgen(start)]
