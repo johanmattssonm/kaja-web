3c1e5fa83450e6965ddce7b88a5916191f45a232
Johan Mattsson <johan.mattsson.m@gmail.com>
2026-06-25 12:32:35 +0200

    Fix prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,16 +1,16 @@
 pub use crate::{
-    get_callback, get_cookie_value, get_element, get_elements, init_callbacks, inner_html,
-    inner_text, try_get_value, try_inner_html, try_inner_text, CallbackRegistration, Error, InitFn,
-    Result,
+    CallbackRegistration, Error, InitFn, Result, get_callback, get_cookie_value, get_element,
+    get_elements, get_value, init_callbacks, inner_html, inner_text, try_get_value, try_inner_html,
+    try_inner_text,
 };
 
 pub use gloo::console::error;
 pub use gloo::console::log;
 pub use serde_wasm_bindgen;
 
-pub use wasm_bindgen::prelude::wasm_bindgen;
 pub use wasm_bindgen::JsCast;
 pub use wasm_bindgen::JsValue;
+pub use wasm_bindgen::prelude::wasm_bindgen;
 pub use wasm_bindgen_futures;
 
 pub use kaja_callback_macro::callback;
